9114b900010dec91b0d5e4ad58bcd7256f896d1f
jecihjoy <jecihjoy@gmail.com>
2023-03-31 16:30:54 +0300

    Tasks component init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
+import Tasks from './components/Tasks'
 import './App.css';
 
 function App() {
   return (
     <div className='container'>
       <Header title="Hellow world"/>
+      <Tasks/>
     </div>
   );
 }
