136f7f024fd15a4b2e64587be7a4ec57ad0c2fea
jecihjoy <jecihjoy@gmail.com>
2024-01-27 02:16:42 +0300

    Using zod validation library

--- src/registration-form/registration-form.component.tsx
+++ src/registration-form/registration-form.component.tsx
@@ -1,14 +1,17 @@
 import { useForm, SubmitHandler } from "react-hook-form";
-import {z} from 'zod';
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type FormProps = {
-  firstName: String;
-  lastName: String;
-  email: String;
-  gender: String;
-  dob: String;
-  password: string;
-};
+const schema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string().email(),
+  gender: z.string(),
+  dob: z.string(),
+  password: z.string().min(3),
+});
+
+type FormProps = z.infer<typeof schema>;
 
 const RegistrationForm = () => {
   const {
@@ -18,7 +21,8 @@ const RegistrationForm = () => {
     formState: { errors, isSubmitting },
   } = useForm<FormProps>({
     mode: "all",
-    defaultValues: {email: 'test@gmail.com', password: 'test123'},
+    defaultValues: { email: "test@gmail.com", password: "test123" },
+    resolver: zodResolver(schema),
   });
   // handleSubmit --> prevents default form behaviour and validates the values
 
@@ -28,7 +32,7 @@ const RegistrationForm = () => {
       await new Promise((resolve) => setTimeout(resolve, 1000));
       throw new Error();
     } catch (err) {
-      setError("email", { message: "This email is already taken" });
+      setError("root", { message: "This email is already taken" });
     }
   };
 
@@ -43,42 +47,21 @@ const RegistrationForm = () => {
           placeholder="First Name"
         />
         <input {...register("lastName")} type="text" placeholder="Last Name" />
-        <input
-          {...register("email", {
-            required: "Email is required",
-            validate: (value) => {
-              if (!value.includes("@")) {
-                return "Email must include @";
-              }
-              return true;
-            },
-          })}
-          type="text"
-          placeholder="Email"
-        />
-        {errors.email && (
-          <div className="text-red-500">{errors.email.message}</div>
-        )}
+        <input {...register("email")} type="text" placeholder="Email" />
         <input {...register("gender")} type="text" placeholder="Gender" />
         <input {...register("dob")} type="text" placeholder="Dob" />
         <input
-          {...register("password", {
-            required: "Password is required",
-            minLength: {
-              value: 3,
-              message: "Password must be more than 3 characters",
-            },
-          })}
+          {...register("password")}
           type="password"
           placeholder="Password"
         />
-        {errors.password && (
-          <div className="text-red-500">{errors.password.message}</div>
-        )}
         <br></br>
         <button disabled={isSubmitting} type="submit">
           {isSubmitting ? "Loading" : "Submit"}
         </button>
+        {errors.root && (
+          <div className="text-red-500">{errors.root.message}</div>
+        )}
       </form>
     </>
   );
